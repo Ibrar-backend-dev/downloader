4cdf236e1988bf8167c717f14c2ca61b2f3f470e
Ibrar-backend-dev <ibrarshahzad.dev@gmail.com>
2026-09-08 20:50:02 +0500

    fix: restore server path dependency
    Restore the path module required when creating the downloads directory during backend startup.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const path = require('path');
 const fs = require('fs-extra');
 const { spawn } = require('child_process');
 const http = require('http');
